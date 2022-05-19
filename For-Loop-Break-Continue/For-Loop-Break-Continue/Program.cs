48c77f70e3598ddc00ed15552b5ec783a6c822c9
Muhammed KILIÇ <105243448+mhmmdklc@users.noreply.github.com>
2022-05-19 20:49:33 +0300

    Add files via upload

---
+++ For-Loop-Break-Continue/For-Loop-Break-Continue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ForLoopBreakContinue
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+
+            int n = 2;
+
+            for (int i=1; i<=10; i++)
+            {
+
+                Console.WriteLine(n + " x " + i + " = " + i * n);
+
+            }
+
+
+
+
+        }
+    }
+}
